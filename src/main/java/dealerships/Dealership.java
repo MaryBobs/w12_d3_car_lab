105a15240b9e1e35b55b1fc23b920796f53fa668
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-15 14:57:36 +0000

    adds car method

--- src/main/java/dealerships/Dealership.java
+++ src/main/java/dealerships/Dealership.java
@@ -1,16 +1,29 @@
 package dealerships;
 
+import cars.Car;
+import cars.PetrolCar;
+
+import java.util.ArrayList;
+
 public class Dealership {
 
-//    private ArrayList<Car> stock;
+    private ArrayList<Car> stock;
     private double till;
 
     public Dealership(double till) {
-//        this.stock = new ArrayList<Car>();
+        this.stock = new ArrayList<Car>();
         this.till = till;
     }
 
     public double getTill() {
         return this.till;
+    }
+
+    public int getStock() {
+        return this.stock.size();
+    }
+
+    public void addCar(Car car) {
+        this.stock.add(car);
     }
 }
--- src/test/java/dealerships/DealershipTest.java
+++ src/test/java/dealerships/DealershipTest.java
@@ -1,5 +1,8 @@
 package dealerships;
 
+import cars.PetrolCar;
+import cars.Car;
+import components.Engine;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,14 +11,29 @@ import static org.junit.Assert.assertEquals;
 public class DealershipTest {
 
     private Dealership dealership;
+    private PetrolCar petrolCar;
+    private Engine engine1;
 
     @Before
     public void before() {
+        engine1 = new Engine(2000);
+        petrolCar = new PetrolCar(10000.00, "blue", "Fiesta", 50, engine1);
         dealership = new Dealership(50000.00);
     }
 
     @Test
     public void canGetTill() {
         assertEquals(50000.00, dealership.getTill(), 0.01);
+    }
+
+    @Test
+    public void canGetStock() {
+        assertEquals(0, dealership.getStock());
+    }
+
+    @Test
+    public void canAddCar() {
+        dealership.addCar(petrolCar);
+        assertEquals(1, dealership.getStock());
     }
 }
